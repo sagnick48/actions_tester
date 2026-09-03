f90aa51175d02cf9d56d261095cbda42e1971e85
Sagnick Das <sagnick.das@nzminds.com>
2026-09-03 17:30:41 +0530

    Containerize && Minor Bug Fixes

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 app = fastapi.FastAPI()
-e=os.genenv("TEST_CODE")
-e1=os.genenv("TEST_CODE1")
+e=os.getenv("TEST_CODE")
+e1=os.getenv("TEST_CODE1")
 @app.get("/")
 def proj_app():
     return {"message":"fastapi test on /"}
 @app.get("/emp")
 def proj_app():
-    return {f"message":"fastapi test on /emp - {e}"}
+    return {"message":f"fastapi test on /emp - {e}"}
 @app.get("/wmp")
 def proj_app2():
-    return {f"message":"fastapi test on /wmp - {e1}"}
+    return {"message":f"fastapi test on /wmp - {e1}"}
